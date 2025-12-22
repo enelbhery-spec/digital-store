edb52c4ea3854847fdb909836cc6254646c0dd80
Ahmed Elbhery <ahmed@gmail.com>
2025-12-22 09:07:30 +0200

    add sitemap

---
+++ src/app/sitemap/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const baseUrl = "https://digetal-app-q1mf.vercel.app";
+
+  const urls = [
+    "",
+    "/products",
+    "/delivery/hotline",
+  ];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls
+  .map(
+    (url) => `
+  <url>
+    <loc>${baseUrl}${url}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`
+  )
+  .join("")}
+</urlset>`;
+
+  return new NextResponse(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
