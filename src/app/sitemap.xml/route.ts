f8f8443f56c1d4cac4719df54d7d0282a5cf9b82
Ahmed Elbhery <ahmed@gmail.com>
2026-07-31 16:56:45 +0300

    update sitemap

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -6,7 +6,7 @@ export const revalidate = 0;
 
 export async function GET() {
   const headersList = await headers();
-  const host = headersList.get("x-forwarded-host") || headersList.get("host") || "https://digetal-app.vercel.app";
+  const host = headersList.get("x-forwarded-host") || headersList.get("host") || "https://digetal-app.vercel.app/eg";
   const baseUrl = `https://${host}`;
   const urls: string[] = [];
 
